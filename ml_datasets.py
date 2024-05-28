5ce19cabe4238037dadbbe56115b13074a13e6a0
ShreyCitrusbug <shrey.citrusbug@gmail.com>
2024-05-28 14:39:19 +0530

    Prompt engineering
    Added docs for prompt engineering

--- ml_datasets.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-# load csv data
-test_csv = pd.read_csv("train.csv")
-print(test_csv.head(), "-> ===== load csv data =====")
-print(test_csv.shape[0], "-> ===== shape csv data =====")
